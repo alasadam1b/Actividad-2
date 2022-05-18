9b6a5f3c0a009f8261f5dd33f7e50fa7f585f5c1
alasadam1b <alasadam1b@gmail.com>
2022-05-18 20:39:44 +0200

    Definición de una única clase por fichero

--- src/ende/Juego.java
+++ src/ende/Juego.java
@@ -3,8 +3,6 @@ package ende;
 /**
  * Simula el juego de Piedra, Papel o tijera
  */
-
-
 public class Juego 
 {
     public static void main(String args[])
@@ -73,40 +71,4 @@ public class Juego
             System.out.println();
         } while(fin_de_juego!=true);
     }
-}
-/**
- *
- */
-class Jugador{
-   
-    /**
-     * Escoge piedra, papel o tijera al azar
-     */
-    public String opcion_al_azar()
-    {
-        String opcion="";
-        Integer c = (int)(Math.random()*3);
-        switch(c){
-            case 0:
-            	opcion=("piedra");
-                break;
-            case 1:
-            	opcion=("papel");
-                break;
-            case 2:
-            	opcion=("tijeras");
-        }
-        return opcion;
-    }
-    public void setÉxitos() 
-    {
-        éxitos++;
-    }
-    public int getÉxitos() 
-    {
-        return(éxitos);
-    }
-    
-    int éxitos;      // número de partidas ganadas
-    int winTotal;
 }
